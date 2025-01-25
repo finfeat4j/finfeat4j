c8ebcd01e786f533e087033a01b0da04bcbdf4bb
eugen <eugene.kamenev@gmail.com>
2025-01-25 11:15:04 +0500

    fix: ds supplier cleanup

--- finfeat4j-ml/src/test/groovy/com/github/finfeat4j/fs/DatasetSupplier.java
+++ finfeat4j-ml/src/test/groovy/com/github/finfeat4j/fs/DatasetSupplier.java
@@ -22,58 +22,14 @@ public class DatasetSupplier implements Supplier<IndicatorSet<Bar>> {
     }
 
     public IndicatorSet<Bar> get() {
-        int winSize = 32;
-        // just for testing purpose, any other combination of indicators can be used
         var indicatorSet = new IndicatorSet<Bar>();
-        //indicatorSet.add(toQuant(new Close()));
         for (int i = 0; i < 31; i+=2) {
-            //   indicatorSet.add(new Close().then(new QuantIndicator(16, 6, 2)));
-           // indicatorSet.add(toQuant(new PGO(i + 2, new Close())));
-            // for (int j = 0; j <= 4; j+=2) {
-            //indicatorSet.add(toQuant(new MFI(i + 2)));
-            //indicatorSet.add(toQuant(new MFI(i + 2)));
-            //indicatorSet.add(toQuant(new Close().then(new ARSI(i + 2))));
-          //  indicatorSet.add(toQuant(new Close().then(new VSCT(i + 2))));
-           // indicatorSet.add(toQuant(new Close().then(new TSI(i + 2))));
             for (int s = 3; s <= 12; s += 4) {
                 indicatorSet.add(toQuant(new WLR(i + 2).then(new NET(s))));
                 indicatorSet.add(toQuant(new Close().then(new ARSI(i + 2)).then(new NET(s))));
-              //  indicatorSet.add(toQuant(new Close().then(new IFTRSI(i + 2, s))));
                 indicatorSet.add(toQuant(new MFI(i + 2).then(new NET(s))));
-               // indicatorSet.add(toQuant(new Close().then(new TSI(i + 2)).then(new PFE(s, 3))));
-               // indicatorSet.add(toQuant(new Close().then(new RSI(i + 2)).then(new PFE(s, 3))));
-            }
-
-            //  }
-            //indicatorSet.add(toQuant(new Close().then(new RSI(i + 2))));
-            //indicatorSet.add(new Close().then(new VST(i + 2)).then(new QuantIndicator(16, 6, 2)));
-            /*for (int j = 0; j < 6; j+=2) {
-                indicatorSet.add(toQuant(new Close().then(new USI(i + 2, j + 2))));
-                // indicatorSet.add(new VAMA(i + 2).then(new RSI(j + 2)).then(new QuantIndicator(16, 6, 2)));
-                indicatorSet.add(toQuant(new MFI(i + 2).then(new PFE(j + 2, 5))));
-                indicatorSet.add(toQuant(new Close().then(new ARSI(i + 2)).then(new PFE(j + 2, 5))));
-                indicatorSet.add(toQuant(new Close().then(new VSCT(i + 2)).then(new PFE(j + 2, 5))));
-                indicatorSet.add(toQuant(new WLR(i + 2).then(new PFE(j + 2, 5))));
-            }*/
-            //indicatorSet.add(new MFI(i + 2).then(new QuantIndicator(16)));
-          //  indicatorSet.add(toQuant(new Close().then(new DPO(i + 2))));
-            /*indicatorSet.add(new Close().then(new Fisher(i + 2)).then(new QuantIndicator(16)));
-            for (int j = 0; j < 12; j+=2) {
-                indicatorSet.add(new Close().then(new Fisher(i + 2)).then(new QuantIndicator(16)));
-            }*/
-        }
-
-        for (int i = 5; i < 10; i+= 5) {
-            for (int slow = 10; slow < 30; slow+=5) {
-                for (int fast = 5; fast < 20; fast+=5) {
-                   // indicatorSet.add(toQuant(new Close().then(new CRSI(slow, i, fast))));
-                    // indicatorSet.add(toQuant(new Close().then(new STC(i, fast, slow, 0.5d))));
-                }
             }
         }
-
-
-        //indicatorSet.add(toQuant(new OBV()));
         indicatorSet.add(new Close().rename("trendPrice"));
         indicatorSet.add(new Close().rename("price"));
         indicatorSet.add(Indicator.of((b) -> -1, "class"));
